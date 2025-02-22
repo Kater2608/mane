c58b8b71ff74ed1aa58061b3f21969aabd0f87f1
Kater2608 <katakarsonova56.com>
2025-02-22 14:57:51 +0700

    OOP start

--- TaskPyhon/spisok.py
+++ TaskPyhon/spisok.py
@@ -1,26 +1,35 @@
-spisok = [10, 40, 20, 30]
-# for element in spisok:
-#     print(element + 2)
-# print(range(0,len(spisok)))
-# for i in range(len(spisok)):
-#     # print(i)
-#     print(spisok[i])
-# for i in enumerate(spisok):
-#     print(i)
-# for id, val in enumerate(spisok):
-#     print(id, val)
-r_obj = range(len(spisok))
-e_obj = enumerate(spisok)
-for i in r_obj:
-    if i == 1:
-        break
-# for i in e_obj:
-#  if i[0] == 1:
-#     break
+# spisok = [10, 40, 20, 30]
+# # for element in spisok:
+# #     print(element + 2)
+# # print(range(0,len(spisok)))
+# # for i in range(len(spisok)):
+# #     # print(i)
+# #     print(spisok[i])
+# # for i in enumerate(spisok):
+# #     print(i)
+# # for id, val in enumerate(spisok):
+# #     print(id, val)
+# r_obj = range(len(spisok))
+# e_obj = enumerate(spisok)
+# for i in r_obj:
+#     if i == 1:
+#         break
+# # for i in e_obj:
+# #  if i[0] == 1:
+# #     break
 
-#  for i in r_obj:
-#     print(i)
+# #  for i in r_obj:
+# #     print(i)
 
-# for i in e_obj:
-#  print(i)
-print(e_obj)
+# # for i in e_obj:
+# #  print(i)
+# print(e_obj)
+spisok=[]
+while True:
+        vvod=str(input("Вводите слова,чтобы завершмть введите 'exit': "))
+        if vvod.lower=="exit":
+            print('Конец')
+            break
+        spisok.append(vvod)
+        SortSpisok=sorted(spisok,key=lambda b: (b[-1], len(b)))
+        print(SortSpisok)
